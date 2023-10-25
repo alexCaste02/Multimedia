310055ecb187fa67d6167bea7d7af4b0571a83ff
Alex <alexcaste2002gs@gmail.com>
2023-10-25 11:29:04 +0200

    finished

--- app/src/main/java/com/example/TiraDados/MainActivity.kt
+++ app/src/main/java/com/example/TiraDados/MainActivity.kt
@@ -6,10 +6,8 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
+import android.media.MediaPlayer
 class MainActivity : AppCompatActivity() {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,35 +22,36 @@ class MainActivity : AppCompatActivity() {
             R.drawable.d6
         )
 
-
         val button: Button = findViewById(R.id.button)
         val imagen: ImageView = findViewById(R.id.topImage)
         val resultTextView: TextView = findViewById(R.id.textView)
 
+        // Initialize MediaPlayer with the sound file
+        var mediaPlayer = MediaPlayer.create(this, R.raw.dice) // Replace "your_sound_file" with your sound file name
+
         button.setOnClickListener {
+
+            mediaPlayer?.start()
 
             object : CountDownTimer(3000, 250) {
 
                 override fun onTick(millisUntilFinished: Long) {
-                    var n = roll()
+                    var n = roll6()
                     imagen.setImageResource(images[n])
-                    resultTextView.text = ((n+1).toString())
-
+                    resultTextView.text = ((n + 1).toString())
                 }
 
                 override fun onFinish() {
-                    var n = roll()
-                    imagen.setImageResource(images[n])
-                    resultTextView.text = (n+1).toString()
 
                 }
 
             }.start()
-
         }
     }
 
-    private fun roll(): Int {
+
+
+    private fun roll6(): Int {
         return (0..5).random()
     }
 }
